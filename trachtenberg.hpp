785a0c25546be350a8023486fd7d04160a78a39c
Mickaël Wolff <mickael@lupusmic.org>
2021-07-25 13:10:12 +0200

    Isolate implementation from interface

--- trachtenberg.hpp
+++ trachtenberg.hpp
@@ -1,74 +1,23 @@
-#include <cstddef>
-#include <cstring>
+#include <memory>
 #include <string>
-#include <utility>
-
-using std::size_t;
-using std::strlen;
 
 // https://en.wikipedia.org/wiki/Trachtenberg_system
 class Trachtenberg
 {
 public:
+	Trachtenberg();
+	~Trachtenberg();
+
 	void multiply(std::string const& a, std::string const& b)
 	{
 		multiply(a.c_str(), b.c_str());
 	}
 
-	void multiply(const char* a, const char* b)
-	{
-		size_t large = strlen(a);
-		size_t small = strlen(b);
-		if (large == 0 || small == 0) {
-			answer = solution = new char[2]{ '0', '\0' };
-			return;
-		}
-		if (large < small) {
-			std::swap(a, b);
-			std::swap(large, small);
-		}
-		solution = new char[large + small];
-		answer = solution + large + small - 1;
-		*answer = '\0'; // zero terminate
-		size_t digits = 0;
-		const char* starta = a + large;
-		const char* startb = b + small;
-		intermediate = 0;
-		while (digits != small) {
-			multiplystep(++digits, --starta, startb);
-		}
-		for (size_t d = digits; d != large; ++d) {
-			multiplystep(digits, --starta, startb);
-		}
-		while (digits != 1) {
-			multiplystep(--digits, starta, --startb);
-		}
-		while (intermediate) {
-			*--answer = '0' + intermediate % 10;
-			intermediate /= 10;
-		}
-	}
-
-	std::string const result() const
-	{
-		return answer;
-	}
-
+	void multiply(const char* a, const char* b);
+	std::string const result() const;
 private:
-	size_t intermediate;
-	char* solution = nullptr;
-	char* answer = nullptr;
+	struct Impl;
+	std::unique_ptr<Impl> pimpl;
 
-	void multiplystep(size_t digits, const char* starta, const char* startb)
-	{
-		const char* digita = starta;
-		const char* digitb = startb;
-		for (size_t n = 0; n != digits; ++n) {
-			--digitb;
-			intermediate += (*digita - '0') * (*digitb - '0'); // convert from ASCII string
-			++digita;
-		}
-		*--answer = '0' + intermediate % 10; // convert back to string
-		intermediate /= 10;
-	}
+	void multiplystep(size_t digits, const char* starta, const char* startb);
 };
